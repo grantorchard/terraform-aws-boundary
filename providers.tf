8a819da0be95a051849ca4aa7826e4fd34ec82dc
Grant Orchard <go@hashicorp.com>
2021-09-07 12:26:20 +1000

    add hcp provider versioning

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.57"
+    }
+		hcp = {
+			source = "hashicorp/hcp"
+			version = "~> 0.15"
+		}
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      owner              = "go"
+      se-region          = "apj"
+      purpose            = "boundary testing"
+      ttl                = "-1"
+      terraform          = true
+      hc-internet-facing = true
+    }
+  }
+}
