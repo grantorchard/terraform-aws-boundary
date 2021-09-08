460bd86879e06209e3d64383b7d8a4ddaa416616
Grant Orchard <go@hashicorp.com>
2021-09-08 17:13:08 +1000

    set all configureable attribute values to variables

--- lb.tf
+++ lb.tf
@@ -1,20 +1,25 @@
-module "boundary_api_lb" {
+module "boundary_controller_lb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "6.5.0"
-  # insert the 4 required variables here
+
   vpc_id  = local.vpc_id
   subnets = local.public_subnets
   security_groups = [
     aws_security_group.lb.id
   ]
 
-  name = "boundary-api"
+  name = "boundary-controller"
 
   https_listeners = [
     {
-      port            = 443
+      port            = var.api_lb_port
       protocol        = "HTTPS"
       certificate_arn = module.boundary_cert.acm_certificate_arn
+    },
+		{
+      port            = var.cluster_lb_port
+      protocol        = "HTTPS"
+      certificate_arn = module.boundary_controller_cert.acm_certificate_arn
     }
   ]
 
@@ -24,7 +29,7 @@ module "boundary_api_lb" {
       protocol    = "HTTP"
       action_type = "redirect"
       redirect = {
-        port        = "443"
+        port        = var.api_lb_port
         protocol    = "HTTPS"
         status_code = "HTTP_301"
       }
@@ -33,17 +38,45 @@ module "boundary_api_lb" {
 
   target_groups = [
     {
+      name_prefix      = "api"
+      backend_protocol = "HTTP"
+      backend_port     = var.api_port
+      target_type      = "instance"
+    },
+		{
       name_prefix      = "cnt"
       backend_protocol = "HTTP"
-      backend_port     = 9200
+      backend_port     = var.cluster_port
       target_type      = "instance"
+    }
+  ]
+}
 
-      targets = [
-        {
-          target_id = module.boundary-controller.id
-          port      = 9200
-        }
-      ]
+module "boundary_worker_lb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "6.5.0"
+
+	load_balancer_type = "network"
+
+  vpc_id  = local.vpc_id
+  subnets = local.public_subnets
+
+  name = "boundary-worker"
+
+  https_listeners = [
+    {
+      port            = var.worker_lb_port
+      protocol        = "TLS"
+      certificate_arn = module.boundary_worker_cert.acm_certificate_arn
+    }
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "wrk"
+      backend_protocol = "TCP"
+      backend_port     = var.worker_port
+      target_type      = "instance"
     }
   ]
 }
@@ -58,5 +91,21 @@ resource "aws_route53_record" "this" {
   name    = "${var.lb_hostname}.${data.aws_route53_zone.this.name}"
   type    = "CNAME"
   ttl     = "300"
-  records = [module.boundary_api_lb.lb_dns_name]
+  records = [module.boundary_controller_lb.lb_dns_name]
+}
+
+resource "aws_route53_record" "controller" {
+  zone_id = data.aws_route53_zone.this.id
+  name    = "${var.controller_lb_hostname}.${data.aws_route53_zone.this.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [module.boundary_controller_lb.lb_dns_name]
+}
+
+resource "aws_route53_record" "worker" {
+  zone_id = data.aws_route53_zone.this.id
+  name    = "${var.worker_lb_hostname}.${data.aws_route53_zone.this.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [module.boundary_worker_lb.lb_dns_name]
 }
