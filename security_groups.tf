c53ae8c1f289e8f3f2adb9126284bf6ae375fbc9
Grant Orchard <go@hashicorp.com>
2021-09-17 13:28:33 +1000

    changes to move controller cluster lb to nlb

--- security_groups.tf
+++ security_groups.tf
@@ -73,7 +73,7 @@ resource "aws_security_group_rule" "controller_ingress_access" {
   from_port                = var.cluster_port
   to_port                  = var.cluster_port
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.controller
+  security_group_id        = aws_security_group.controller.id
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
