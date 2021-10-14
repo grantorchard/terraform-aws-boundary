9394631226e656a1c16f1ea16bea58302bde060a
Grant Orchard <go@hashicorp.com>
2021-10-14 18:06:40 +1100

    include egress for controllers to temporary vault server

--- security_groups.tf
+++ security_groups.tf
@@ -112,7 +112,7 @@ resource "aws_security_group_rule" "controller_to_vault" {
   from_port         = 8200
   to_port           = 8200
   protocol          = "tcp"
-  security_group_id = aws_security_group.controller.id
+  source_security_group_id = aws_security_group.controller.id
 	cidr_blocks = [
 		"172.25.16.0/20",
 		"10.0.101.71/32"
